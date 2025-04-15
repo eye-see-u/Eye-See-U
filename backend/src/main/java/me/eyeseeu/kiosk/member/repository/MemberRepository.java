3c9eff6f12cbe19bdf7b0a2ef7eb31fe627d7e7d
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2025-04-15 21:36:44 +0900

    feat: 회원 repository 추가

---
+++ backend/src/main/java/me/eyeseeu/kiosk/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package me.eyeseeu.kiosk.member.repository;
+
+import me.eyeseeu.kiosk.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}

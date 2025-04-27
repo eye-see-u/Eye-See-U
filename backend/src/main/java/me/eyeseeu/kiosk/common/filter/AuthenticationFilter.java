8b812d7a3940d2e1ba80fb923c6760d5a18707aa
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2025-04-27 17:01:53 +0900

    feat: 인증 확인 필터 추가

---
+++ backend/src/main/java/me/eyeseeu/kiosk/common/filter/AuthenticationFilter.java
@@ -0,0 +1,49 @@
+package me.eyeseeu.kiosk.common.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebFilter(filterName = "authenticationFilter", urlPatterns = "/api/*")
+public class AuthenticationFilter implements Filter {
+
+    private static final List<String> WHITE_LIST = Arrays.asList(
+        "/api/user/login",
+        "/api/user/signup",
+        "/api/user/logout"
+    );
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String requestURI = httpRequest.getRequestURI();
+        if (WHITE_LIST.contains(requestURI)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Object memberId = httpRequest.getSession().getAttribute("memberId");
+
+        if (memberId == null) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.setCharacterEncoding("UTF-8");
+            httpResponse.setContentType("application/json");
+            httpResponse.getWriter().write("{\"message\": \"세션이 유효하지 않습니다.\"}");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}

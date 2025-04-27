afd761b986bfa74c4b4888574c1a4e5c606b6a99
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2025-04-27 14:57:26 +0900

    feat: 로그아웃 기능 구현

--- backend/src/main/java/me/eyeseeu/kiosk/member/controller/AuthController.java
+++ backend/src/main/java/me/eyeseeu/kiosk/member/controller/AuthController.java
@@ -39,4 +39,9 @@ public class AuthController {
 
         return memberInfo;
     }
+
+    @PostMapping("/logout")
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
 }

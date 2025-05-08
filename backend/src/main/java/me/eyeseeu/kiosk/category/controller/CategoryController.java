afb76d7118fd129948db69a8ac8972b86d8b790a
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2025-05-08 15:14:39 +0900

    style: 코드 리포멧

--- backend/src/main/java/me/eyeseeu/kiosk/category/controller/CategoryController.java
+++ backend/src/main/java/me/eyeseeu/kiosk/category/controller/CategoryController.java
@@ -11,7 +11,6 @@ import me.eyeseeu.kiosk.category.dto.response.CategoryGetResponse;
 import me.eyeseeu.kiosk.category.dto.response.CategoryUpdateResponse;
 import me.eyeseeu.kiosk.category.service.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- backend/src/main/java/me/eyeseeu/kiosk/category/dto/request/CategoryCreateRequest.java
+++ backend/src/main/java/me/eyeseeu/kiosk/category/dto/request/CategoryCreateRequest.java
@@ -1,7 +1,6 @@
 package me.eyeseeu.kiosk.category.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record CategoryCreateRequest(
     @NotBlank(message = "카테고리 이름은 필수입니다.")
